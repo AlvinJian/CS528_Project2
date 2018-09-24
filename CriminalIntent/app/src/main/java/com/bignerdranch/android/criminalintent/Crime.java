b9844f36616ab71ea9f1c383bdb1dd19293fadcc
Jonathan Wang <wangjonathan1992@gmail.com>
2018-09-24 18:26:09 -0400

    add four new attributes for image uri

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -10,6 +10,10 @@ public class Crime {
     private Date mDate;
     private boolean mSolved;
     private String mSuspect;
+    private String img_1;
+    private String img_2;
+    private String img_3;
+    private String img_4;
 
     public Crime() {
         this(UUID.randomUUID());
@@ -57,5 +61,37 @@ public class Crime {
 
     public String getPhotoFilename() {
         return "IMG_" + getId().toString() + ".jpg";
+    }
+
+    public String getImg_1() {
+        return img_1;
+    }
+
+    public void setImg_1(String img_1) {
+        this.img_1 = img_1;
+    }
+
+    public String getImg_2() {
+        return img_2;
+    }
+
+    public void setImg_2(String img_2) {
+        this.img_2 = img_2;
+    }
+
+    public String getImg_3() {
+        return img_3;
+    }
+
+    public void setImg_3(String img_3) {
+        this.img_3 = img_3;
+    }
+
+    public String getImg_4() {
+        return img_4;
+    }
+
+    public void setImg_4(String img_4) {
+        this.img_4 = img_4;
     }
 }
